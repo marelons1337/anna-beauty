ef25de1bd2633df15a87d1665a4bca3b993ccb3a
Marelons1337 <jarorafo@gmail.com>
2020-10-09 00:39:59 +0200

    added carousel

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const thumbnail = document.getElementById("thumbnail");
+const modalBox = document.getElementById("modalBox");
+
+thumbnail.onclick = function() {
+    modalBox.style.visibility = "visible";
+    console.log("visible");
+    
+}
+modalBox.onclick = function() {
+    modalBox.style.visibility = "hidden"
+}
 
 
 
